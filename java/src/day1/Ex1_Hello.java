c7d619fae8ee61ce29723ae46232730aa080a58f
chosub1n <104331438+chosub1n@users.noreply.github.com>
2022-04-25 13:57:22 +0900

    자바 1일차 : Hello world 출력 예제

---
+++ java/src/day1/Ex1_Hello.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Ex1_Hello {
+	//콘솔 응용 프롬그램이 실행되기 위해서 main이라는 메소드가 필요하다.
+	public static void main(String[] args) {
+	/*여러줄 주석
+	 * 주석1
+	 * 주석2
+	 * */
+	//한 줄 주석은 아무 위치에서 사용가능 하지만 중간은 안 됨
+	/* println(문자열) : 문자열을 콘솔에 출력한 후 줄바꿈을 함. (엔터 효과)
+	 * print(문자열) : 문자열을 콘솔에 출력
+	 **/
+		System.out.print("Heloo world!!");
+        System.out.println("Hello world!!"); // 한 줄 주석은, 주석기호가 나온 시점부터 해당 라인 끝까지
+        System.out.print("Heloo world!!");
+	}
+}
