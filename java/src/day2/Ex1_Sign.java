f71adf43eac05702be2ee54b9c39b2ead41fe685
chosub1n <104331438+chosub1n@users.noreply.github.com>
2022-04-26 12:31:04 +0900

    자바 2일차 : 부호 연산자 예제

---
+++ java/src/day2/Ex1_Sign.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class Ex1_Sign {
+
+	public static void main(String[] args) {
+		//정수형 변수 num1를 선언 후 1로 초기화
+		int num1 = -1;
+		System.out.println(num1);
+		System.out.println(-num1);
+		
+
+	}
+
+}
