7d8f2e93478a4ebe041c280d99155d55bedad30e
chosub1n <104331438+chosub1n@users.noreply.github.com>
2022-04-29 17:11:01 +0900

    자바 5일차 : 중첩 반복문 예제 - 별 출력 예제

---
+++ java/src/day5/Ex6_NestedLoop1.java
@@ -0,0 +1,28 @@
+package day5;
+
+public class Ex6_NestedLoop1 {
+
+	public static void main(String[] args) {
+		
+		/* *****
+		 * *****
+		 * *****
+		 * *****
+		 * *****
+		 * 
+		 * */
+		
+		for(int i = 1; i <=5; i++) {
+			// * 5개 출력하고 엔터치는 코드
+			//System.out.println("*****");
+			// * 5개 출력하고 엔터피는 반복문 코드로 수정
+			// *을 5개 반복문으로 한 줄에 출력
+		for(int j = 1; j <=5 ; j++) {
+			System.out.print("*");
+		}
+		//엔터
+	    System.out.println();
+		}
+	}
+
+}
