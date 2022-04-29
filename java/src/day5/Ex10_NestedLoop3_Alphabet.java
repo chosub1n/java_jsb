382440dbc54fcd11207bd18a361c86605237f76c
chosub1n <104331438+chosub1n@users.noreply.github.com>
2022-04-29 17:15:06 +0900

    자바 5일차 : 중첩 반복문 예제 - 알파벳 출력 예제

---
+++ java/src/day5/Ex10_NestedLoop3_Alphabet.java
@@ -0,0 +1,31 @@
+package day5;
+
+public class Ex10_NestedLoop3_Alphabet {
+
+	public static void main(String[] args) {
+		/* a				'a'부터 'a'까지
+		 * ab				'a'부터 'b'까지
+		 * abc				
+		 * ...
+		 * abcdefg...xyz	'a'부터 'z'까지
+		 * 반복횟수 : end는 'a'부터 'z'까지 1씩 증가
+		 * 규칙성 : ch는 'a'부터 end까지 1씩 증가하며 ch를 출력
+		 * */
+		
+
+		for(char end = 'a'; end <= 'z'; end++) {
+			System.out.print(end);
+			/* ch는 'a'부터 end까지 1씩 증가하며 ch를 출력
+			 * 
+			 * 반복횟수 : ch는 'a'부터 end까지 1씩 증가
+			 * 규칙성 : ch를 출력
+			 * */
+			for(char ch = 'a'; ch <= end; ch++) {
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
