3882efd112d1760225b5da5c79729539f670f371
chosub1n <104331438+chosub1n@users.noreply.github.com>
2022-05-20 00:01:49 +0900

    자바 18일차 : List를 이용한 숫자 야구 게임 프로그램 예제

--- java/src/day14/Ex2_BaseballGameMain.java
+++ java/src/day14/Ex2_BaseballGameMain.java
@@ -6,7 +6,7 @@ public class Ex2_BaseballGameMain {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		BaseballGame1 game = new BaseballGame1(scan);
+		BaseballGame game = new BaseballGame(scan);
 		game.run();
 		scan.close();
 
