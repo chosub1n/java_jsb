4a10b6df0afc2b9a95d1cd6ec4563ee752969d5e
chosub1n <104331438+chosub1n@users.noreply.github.com>
2022-06-06 19:57:29 +0900

    DB 4일차 : cookDB - 검색 예제

---
+++ DB/DB 4일차_02_cookDB_검색 예제.sql
@@ -0,0 +1,43 @@
+-- 각 회원이 구매한 총 개수
+-- sum(속성명) : 속성명에 있는 값을 누적합
+-- sum(속성명)이 order by와 같이 사용되면 그룹화한 속성을 기준으로 누적합
+SELECT userId, sum(amount) as 구매량 FROM buytbl group by userID;
+
+-- 각 회원이 구매한 횟수를 조회
+select userId, count(userId) FROM buytbl group by userID;
+
+-- 각 제품을 산 회원들의 수
+select prodName, count(distinct userId) from buytbl group by prodName;
+
+-- 각 구매별 구매 금액을 조회
+select *, price * amount as 구매금액 from buytbl;
+
+-- 회원별 구매 총 금액을 조회
+select userId, sum(price*amount) as `총 구매금액` from buytbl group by userId;
+
+-- 회원별 구매 총 금액이 500원 이상인 구매 내역을 조회
+select userID, sum(price*amount) as `총 구매금액` 
+	from buytbl 
+	group by userId 
+    having `총 구매금액` >= 500;
+
+-- 가장 많이 구매한 회원의 아이디를 조회
+select userId, sum(price*amount) as `총 구매금액` from buytbl 
+	group by userId
+    order by `총 구매금액` desc
+    limit 1;
+    
+-- 경남에 사는 회원의 수를 조회
+select addr, count(userID) from usertbl where addr = '경남';
+
+-- 각 지역에 사는 회원의 수를 조회
+select addr, count(userID) as `회원 수` from usertbl group by addr;
+
+-- 경남지역에 사는 회원이 구매한 제품 목록을 조회
+select userID, prodName from buytbl 
+	where userId = any(select userId from usertbl where addr = '경남');
+select userID, prodName from buytbl 
+	where userId in(select userId from usertbl where addr = '경남');
+
+
+SELECT * FROM cookdb.buytbl;
