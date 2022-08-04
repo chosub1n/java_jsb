2982516532799c3d61d5fc9b83551ef94d1f7c3f
chosub1n <104331438+chosub1n@users.noreply.github.com>
2022-08-04 14:46:00 +0900

    spring : lombok 추가 및 DB 연동

---
+++ spring_jsb/src/main/java/kr/green/spring/VO/MemberVO.java
@@ -0,0 +1,16 @@
+package kr.green.spring.VO;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	
+	private String me_id;
+	private String me_pw;
+	private char me_gender;
+	private Date me_birth;
+	private String me_email;
+	private int me_authority;
+}
