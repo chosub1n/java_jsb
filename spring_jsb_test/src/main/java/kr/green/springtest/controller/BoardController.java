6bea2290a7245be469ffe326a1eef27750a5edc9
chosub1n <104331438+chosub1n@users.noreply.github.com>
2022-08-16 16:21:26 +0900

    spring 복습 : 게시글 추천/비추천 기능 구현 완료

--- spring_jsb_test/src/main/java/kr/green/springtest/controller/BoardController.java
+++ spring_jsb_test/src/main/java/kr/green/springtest/controller/BoardController.java
@@ -1,16 +1,21 @@
 package kr.green.springtest.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import kr.green.springtest.vo.BoardVO;
+import kr.green.springtest.vo.LikesVO;
 import kr.green.springtest.vo.MemberVO;
 import kr.green.springtest.pagination.Criteria;
 import kr.green.springtest.pagination.PageMaker;
@@ -36,9 +41,15 @@ public class BoardController {
 	
 	@RequestMapping(value="/board/select/{bd_num}", method=RequestMethod.GET)
     public ModelAndView boardSelectGet(ModelAndView mv,  
-    		@PathVariable("bd_num")int bd_num){
+    		@PathVariable("bd_num")int bd_num, HttpSession session){
 		boardService.updateViews(bd_num);
 		BoardVO board = boardService.getBoard(bd_num);
+		
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		
+		LikesVO likes = boardService.getLikes(bd_num, user);
+		
+		mv.addObject("likes", likes);
 		mv.addObject("board", board);
 		mv.setViewName("/board/select");
 		return mv;
@@ -82,4 +93,16 @@ public class BoardController {
 		mv.setViewName("redirect:/board/list");
 		return mv;
     }
+
+	@RequestMapping("/check/likes")
+	@ResponseBody
+	public Map<Object, Object> checkLikes(@RequestBody LikesVO likes, 
+			HttpSession session){
+	    HashMap<Object, Object> map = new HashMap<Object, Object>();
+	    MemberVO user = (MemberVO)session.getAttribute("user");
+	    //state : 1, -1, 10, -10
+	    String state = boardService.getLikesState(likes, user);
+	    map.put("state", state);
+	    return map;
+	}
 }
