ad94715c80f091102bcaf5e5ccca8dc9957fa640
chosub1n <104331438+chosub1n@users.noreply.github.com>
2022-09-07 16:18:28 +0900

    LG - 공통 : 공지사항 상세 확인 기능 구현 완료

--- LG site/LG/src/main/java/kr/green/lg/controller/BoardController.java
+++ LG site/LG/src/main/java/kr/green/lg/controller/BoardController.java
@@ -1,5 +1,7 @@
 package kr.green.lg.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.green.lg.pagination.Criteria;
+import kr.green.lg.pagination.PageMaker;
 import kr.green.lg.service.BoardService;
 import kr.green.lg.service.MessageService;
+import kr.green.lg.vo.BoardVO;
 import kr.green.lg.vo.MemberVO;
 
 public class BoardController {
@@ -30,6 +35,13 @@ public class BoardController {
 			messageService.message(response, "게시글이 삭제되었습니다.", redirectUrl);
 		else
 			messageService.message(response, "게시글 삭제에 실패했습니다.", redirectUrl);
+		return mv;
+	}
+	@RequestMapping(value = "/board/select", method = RequestMethod.GET)
+	public ModelAndView boardSelectGet(ModelAndView mv, Integer bd_num) {
+		BoardVO board = boardService.getBoard(bd_num);
+		mv.addObject("bo", board);
+		mv.setViewName("board/select");
 		return mv;
 	}
 
